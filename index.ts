3e4c09593bc025002357382632acf98d2dc99815
Richmond Coca <richmond.coca@codeandtheory.com>
2022-10-18 05:57:32 +0800

    eror

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import express, { Express, Request, Response } from 'express';
-import { initYtdlpwrap } from './test';
+// import { initYtdlpwrap } from './test';
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
@@ -8,15 +8,15 @@ app.get('/', async (req: Request, res: Response) => {
     res.send('TEST ONLY');
 })
 
-app.get('/test', async (req: Request, res: Response) => {
-    let response: string | unknown;
-    try {
-        response = await initYtdlpwrap();
-    } catch (error) {
-        response = error;
-    }
-    res.json(await response);
-})
+// app.get('/test', async (req: Request, res: Response) => {
+//     let response: string | unknown;
+//     try {
+//         response = await initYtdlpwrap();
+//     } catch (error) {
+//         response = error;
+//     }
+//     res.json(await response);
+// })
 
 
 app.listen(port, () => {
